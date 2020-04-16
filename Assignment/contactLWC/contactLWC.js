86c456e79cdc7ec132a9c4fc8c1840d59e45799e
agrawalankit1994 <52730873+agrawalankit1994@users.noreply.github.com>
2020-04-16 17:36:16 +0530

    Update contactLWC.js

--- Assignment/contactLWC/contactLWC.js
+++ Assignment/contactLWC/contactLWC.js
@@ -1,34 +1,8 @@
-import { LightningElement, api } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import NAME_FIELD from '@salesforce/schema/Contact.Name';
-import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
-import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
-import FAX_FIELD from '@salesforce/schema/Contact.Fax';
+import { LightningElement, track } from 'lwc';
 
-export default class createContactRecord extends LightningElement
- {
-    
-    @api objectApiName = "Contact";
-
-    fields = [NAME_FIELD, EMAIL_FIELD, PHONE_FIELD, FAX_FIELD];
-
-    handleSuccess(event)
-	{
-        const evt = new ShowToastEvent({
-            title: "Contact created",
-            message: "Record ID: " + event.detail.id,
-            variant: "success"
-        });
-        this.dispatchEvent(evt);
-    }
-
-    handleFailure(event)
-	{
-        const evt = new ShowToastEvent({
-            title: "Error!!",
-            message: event.detail.error.message,
-            variant: "failure"
-        });
-        this.dispatchEvent(evt);
-    }
+export default class createRecordForm extends LightningElement {
+   @track successMessage;
+   handleSuccess(event) {
+    this.successMessage = 'Record Successfully created with Id: '+event.detail.id;
+   }
 }
