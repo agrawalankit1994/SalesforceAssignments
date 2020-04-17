457b3c835d1c12ce8418c77ca6831874cbe483d2
agrawalankit1994 <52730873+agrawalankit1994@users.noreply.github.com>
2020-04-17 18:21:06 +0530

    Update contactLWC.js

--- Assignment/contactLWC/contactLWC.js
+++ Assignment/contactLWC/contactLWC.js
@@ -1,6 +1,11 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+import Contact from '@salesforce/schema/Contact'
 
 export default class createRecordForm extends LightningElement {
+   @api objectApiName = Contact;
+   fields = {'email':EMAIL_FIELD,'phone':PHONE_FIELD};
    @track successMessage;
    handleSuccess(event) {
     this.successMessage = 'Record Successfully created with Id: '+event.detail.id;
